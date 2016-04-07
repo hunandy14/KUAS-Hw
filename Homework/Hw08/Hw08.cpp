45c083a9278186064a17b1233be1d1650a6915d1
CharlotteHonG <hunandy14@gmail.com>
2016-04-07 09:00:39 -0500

    add picture and update hw08
    爆炸惹QQ
    定義失敗，一開始的紙本就有些錯誤了，紙本的標記那些是錯的，判斷是否執行2應該是"不重複上一個步驟"，而不是那些標記
    [5-20][5-29]這兩個步驟同時符合標記條件，不曉得如何判別與上一步一樣
    如果是整個複製一份，可能還有得玩，不曉得是不是最佳解
    還有一種就是標記發牌發到哪裡，想一下哪一個好惹

--- Homework/Hw08/Hw08.cpp
+++ Homework/Hw08/Hw08.cpp
@@ -80,9 +80,30 @@ void node_short(node* nh,int p);
 int hanoi_checkmove(node* nh1, node* nh2);
 // 河內塔移動n1移動到n2 (柱子鏈接開頭,柱子鏈接開頭)
 void hanoi_move(node* nh1, node* nh2);
+// 查詢總數 (河內塔地址)
+int hanoi_lenth(node** ht);
 // 發牌 (河內塔地址)
-void hanoi_sent(node** ht){
-    
+void hanoi_sent(node** ht);
+// 收牌 (河內塔地址,模式)
+// 模式=0(小收),模式=1(大收)
+void hanoi_receive(node** ht, int mode);
+// 小收牌 (河內塔地址)
+void hanoi_receive(node** ht);
+// 手牌差計算 (河內塔地址,模式)
+// 模式=-1 回傳 ht[1]-ht[2] 負值
+int hanoi_handdif(node** ht, int mode);
+// 手牌差[正數] (河內塔地址)
+int hanoi_handdif(node** ht);
+// 組牌 (河內塔地址)
+void hanoi_defrag(node** ht);
+// 換牌 (河內塔地址)
+void hanoi_change(node** ht);
+// 檢查可否發牌
+int hanoi_checksent(node** ht){
+    if (hanoi_checkmove(ht[0],ht[2]) == 1 || \
+        hanoi_checkmove(ht[0],ht[1]) == 1)
+        return 1;
+    return 0;
 }
 //=========================================================
 int main(int argc, char const *argv[]){
@@ -93,40 +114,145 @@ int main(int argc, char const *argv[]){
     ht[1] = node_creat(-1);
     ht[2] = node_creat(-1);
     /* 批次匯入節點 */
-    int len = 4, *data = new int[len];
+    int len = 6, *data = new int[len];
     for (int i = 0; i < len; ++i)
         data[i] = len-i;
     nodep_input(ht[0], data, len);
     //=====================================================
-    hanoi_sent(ht);
+    // continue 
+    int def=1, cha=1, sen=1, rec=1;
+    int rc=0;//下下回之後把上面全部歸零
+    int senc=-1;//第一柱原本有的還沒發完，不要啟動次要條件
+
+    /*第29步失敗，無法精準確定條件
+    可能要記錄上一步的步驟，判定是否相同*/
+
+    int setup=2;
+    for (int i = 0; i < len-1; ++i){
+        setup*=2;
+    }setup--;
+
+    for (int i = 0; i < setup; ++i){
+        cout << "******************" << endl;
+        if (def!=1 || cha!=1 || sen!=1 || rec!=1){
+            rc++;
+        }
+        if (rc==2){
+            rc=0;def=1;cha=1;sen=1;rec=1;
+        }
+        for (int j = 0; j < 3; ++j)
+            node_printalldata(ht[j]);
+        cout << "def=" << def << ",cha=" << cha << ",sen=" << sen; 
+        cout << ",rec=" << rec << ",rc=" << rc << endl;
+
+
+        //偵測第一柱是否發完了
+        if (senc == 0)  //延遲一回合，避免發牌當回合回收
+            senc++;
+        if (node_lenth(ht[0]) ==0 && senc<=0){
+            senc++;
+            // cout << "           ht[0] len =0" << endl;
+        }
+        
+
+        /* 1.組牌 */
+        if (hanoi_handdif(ht)==1 && def==1){
+            hanoi_defrag(ht);
+            cout << "[" << i+1 << "]";
+            cout << "def" << endl;
+            continue;
+        }
+        /* 2.發牌 */
+        if (hanoi_checksent(ht)==1 && sen==1){
+            hanoi_sent(ht);
+            rec=0; rc=0;
+            cout << "[" << i+1 << "]";
+            cout << "sen" << endl;
+            continue;
+        }
+        /* 3.收or換 */
+        // 差 2(雙) => 小收回
+        if (hanoi_handdif(ht) % 2 == 0){
+            sen=0; rc=0;//標記不可發
+            def=1;     //歸零重組
+            cout << "                            ht0 len==" << node_lenth(ht[0]) << endl;
+            if(senc == 1){
+                
+                if(rec==1 || i==28){ //這個i有問題，條件不完全應該會出bug
+                    cout << "               small" << endl;
+                    hanoi_receive(ht);
+                }
+                else {    //已標不可收 => 大收回
+                    cout << "                            big"<<node_lenth(ht[0]) << endl;
+                    cout << i << endl;
+                    hanoi_receive(ht, 1);
+                }
+            }
+            else{
+                cout << "               small*" << endl;
+                hanoi_receive(ht);
+            }
+
+
+            cout << "[" << i+1 << "]";
+            cout << "dif.2" << endl;
+            continue;
+        }
+        // 差 3(單) => 小換
+        if (hanoi_handdif(ht) != 1){
+            if (hanoi_handdif(ht) % 2 == 1){
+                hanoi_change(ht);
+                def=0; rc=0; //標記不可組
+                sen=1; rec=1;      //歸零發收牌
+                cout << "[" << i+1 << "]";
+                cout << "dif.3" << endl;
+                continue;
+            }
+        }else{
+            // 差 1 => 大收回
+            hanoi_receive(ht, 1);
+            cout << "[" << i+1 << "]";
+            cout << "dif.1" << endl;
+            sen=0; rc=0; //標記不可發
+            continue;
+        }
+
+
+
+
+        cout << "[" << i+1 << "]";
+        cout << "Nothing" << endl;
+    }
+
+
+
+
+
+
     //=====================================================
     /* 印出節點 */
     cout << "=====================================" <<endl;
-    node_printalldata(ht[0]); 
-    node_printalldata(ht[1]); 
-    node_printalldata(ht[2]); 
+    for (int i = 0; i < 3; ++i)
+        node_printalldata(ht[i]);
     //=====================================================
     /* 釋放記憶體 */
-    node_deleteall(ht[2]);
-    node_deleteall(ht[1]);
-    node_deleteall(ht[0]);
-    delete [] ht[2];
-    delete [] ht[1];
-    delete [] ht[0];
-    delete [] ht;
+    for (int i = 2; i >= 0; --i){
+        node_deleteall(ht[i]);
+        delete [] ht[i];
+    } delete [] ht;
     return 0;
 }
 //=========================================================
 int hanoi_checkmove(node* nh1, node* nh2){
     if (nodep_final(nh1)->link == NULL){
-        cout << "hanoi_move Error!!" << endl;
-        cout << "   n1 is link head." << endl;
+        // cout << "hanoi_move Error!!" << endl;
+        // cout << "   n1 is link head." << endl;
         return 0;
     }
     if (nodep_final(nh2)->link != NULL){
         if (nodep_final(nh1)->data > nodep_final(nh2)->data){
-            cout << "hanoi_move Error!!" << endl;
-            cout << "   n1 value > n2." << endl;
+            // cout << "hanoi_move Error!!" << endl;
+            // cout << "   n1 value > n2." << endl;
             return 0;
         }
     }
@@ -139,6 +265,92 @@ void hanoi_move(node* nh1, node* nh2){
     node* temp = nodep_final(nh1);
     nodep_unlink(temp);
     nodep_link(nodep_final(nh2), temp);
+}
+
+int hanoi_lenth(node** ht){
+    int len=0;
+    for (int i = 0; i < 3; ++i)
+        len += node_lenth(ht[i]);
+    return len;
+}
+
+void hanoi_sent(node** ht){
+    // 單數
+    if (hanoi_lenth(ht)%2 == 1){
+        if (hanoi_checkmove(ht[0],ht[2]) == 1){
+            hanoi_move(ht[0],ht[2]);
+        }else if (hanoi_checkmove(ht[0],ht[1]) == 1){
+            hanoi_move(ht[0],ht[1]);
+        }else {
+            cout << "hanoi_sent Error!" << endl;
+        }
+    }
+    // 雙數
+    if (hanoi_lenth(ht)%2 == 0) {
+        if (hanoi_checkmove(ht[0],ht[1])==1){
+            hanoi_move(ht[0],ht[1]);
+        }else if (hanoi_checkmove(ht[0],ht[2]) == 1){
+            hanoi_move(ht[0],ht[2]);
+        }else{
+            cout << "hanoi_sent Error!" << endl;
+        }
+    }
+    
+}
+
+void hanoi_receive(node** ht, int mode){
+    if (mode==0){ //小收回
+        if (nodep_final(ht[1])->data > \
+            nodep_final(ht[2])->data){
+            hanoi_move(ht[2],ht[0]);
+        }else{
+            hanoi_move(ht[1],ht[0]);
+        }
+        return;
+    }
+    if (mode==1){ //大收回
+        if (nodep_final(ht[1])->data < \
+            nodep_final(ht[2])->data){
+            hanoi_move(ht[2],ht[0]);
+        }else{
+            hanoi_move(ht[1],ht[0]);
+        }
+        return;
+    }
+}
+
+void hanoi_receive(node** ht){
+    return hanoi_receive(ht, 0);
+}
+
+int hanoi_handdif(node** ht, int mode){
+    int temp=nodep_final(ht[1])->data;
+    temp-=nodep_final(ht[2])->data;
+    if (temp < 0 && mode!=-1)
+        temp += (0 - temp)*2;
+    return temp;
+}
+
+int hanoi_handdif(node** ht){
+    return hanoi_handdif(ht, 0);
+}
+
+void hanoi_defrag(node** ht){
+    if (hanoi_handdif(ht,-1) == 1)
+        hanoi_move(ht[2],ht[1]);
+    else if (hanoi_handdif(ht,-1) == -1)
+        hanoi_move(ht[1],ht[2]);
+    else
+        cout << "defrag Error!" << endl;
+}
+
+void hanoi_change(node** ht){
+    if (hanoi_handdif(ht,-1) > 0)
+        hanoi_move(ht[2],ht[1]);
+    else if (hanoi_handdif(ht,-1) < 0)
+        hanoi_move(ht[1],ht[2]);
+    else
+        cout << "change Error!" << endl;
 }
 //=========================================================
 node* nodep_unlink(node* n2){
@@ -384,12 +596,12 @@ int node_value(node* nh, int p){
 }
 
 void node_short(node* nh, int h, int f){
-    int i=h, j=h+1;
-    for ( j; j<=f; j++)
-        for(i=j-1; i>=h && node_value(nh,i)>node_value(nh,i+1); i--){
+    for (int j=h+1; j<=f; j++){
+        for(int i=j-1; i>=h && node_value(nh,i)>node_value(nh,i+1); i--){
             //如果比前面大就一直往前換，直到比前面小
             nodep_change(node_address(nh,i),node_address(nh,i+1));
         }
+    }
 }
 
 void node_short(node* nh){
