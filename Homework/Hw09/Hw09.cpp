f86088d5e148313ba5d1095088f807bd478db837
CharlotteHonG <hunandy14@gmail.com>
2016-05-08 20:56:39 +0800

    整理函數，拉到最底下

--- Homework/Hw09/Hw09.cpp
+++ Homework/Hw09/Hw09.cpp
@@ -3,8 +3,6 @@ Name:KUAS-Hw/Hw09
 DATE:2016/05/02
 By CharlotteHong
 **********************************************************/
-
-//=========================================================
 #include <iostream>
 #include <string>
 using namespace std;
@@ -33,58 +31,11 @@ string MainMenu("\
 //=========================================================
 void print_menu();
 void input_st(st* stru, const string name, \
-    const string num, int point){
-    stru->name=name;
-    stru->num=num;
-    stru->point=point;
-    stru->active=1;
-}
-void print_st(st* stru){
-    cout << "名字 = " << stru->name;
-    cout << ", 學號 = " << stru->num;
-    cout << ", 分數 = " << stru->point << endl;
-}
-void print_multist(int len, st* stru){
-    st* temp = stru;
-    for (int i = 0; i < len; ++i){
-        while (1){
-            if (temp->active==2)
-                break;
-            if (temp->active==1)
-                break;
-            temp++;
-        }
-        if (temp->active==2)
-            break;
-        cout << "[" << i << "]";
-        cout << "名字 = " << temp->name;
-        cout << ", 學號 = " << temp->num;
-        cout << ", 分數 = " << temp->point << endl;
-        temp++;
-    }
-}
-void print_multist(st* stru){
-    print_multist(stlen,stru);
-}
-void st_delete(st* stru,int id){
-    st* temp = stru;
-    for (int i = 0; i < stlen; ++i){
-        while (1){
-            if (temp->active==2)
-                break;
-            if (temp->active==1)
-                break;
-            temp++;
-        }
-        if (temp->active==2)
-            break;
-
-        if (i==id){
-            temp->active=0;
-        }
-        temp++;
-    }
-}
+    const string num, int point);
+void print_st(st* stru);
+void print_multist(int len, st* stru);
+void print_multist(st* stru);
+void st_delete(st* stru,int id);
 //=========================================================
 int main(int argc, char const *argv[]){
     char choose=0;
@@ -145,5 +96,58 @@ int main(int argc, char const *argv[]){
 void print_menu(){
     cout << MainMenu;
     cout << "Your choose : ";
+}
+void input_st(st* stru, const string name, \
+    const string num, int point){
+    stru->name=name;
+    stru->num=num;
+    stru->point=point;
+    stru->active=1;
+}
+void print_st(st* stru){
+    cout << "名字 = " << stru->name;
+    cout << ", 學號 = " << stru->num;
+    cout << ", 分數 = " << stru->point << endl;
+}
+void print_multist(int len, st* stru){
+    st* temp = stru;
+    for (int i = 0; i < len; ++i){
+        while (1){
+            if (temp->active==2)
+                break;
+            if (temp->active==1)
+                break;
+            temp++;
+        }
+        if (temp->active==2)
+            break;
+        cout << "[" << i << "]";
+        cout << "名字 = " << temp->name;
+        cout << ", 學號 = " << temp->num;
+        cout << ", 分數 = " << temp->point << endl;
+        temp++;
+    }
+}
+void print_multist(st* stru){
+    print_multist(stlen,stru);
+}
+void st_delete(st* stru,int id){
+    st* temp = stru;
+    for (int i = 0; i < stlen; ++i){
+        while (1){
+            if (temp->active==2)
+                break;
+            if (temp->active==1)
+                break;
+            temp++;
+        }
+        if (temp->active==2)
+            break;
+
+        if (i==id){
+            temp->active=0;
+        }
+        temp++;
+    }
 }
 //=========================================================
