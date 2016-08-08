5cc361ea323122601d482b0c9040572cceb202a5
Charlotte.HonG <hunandy14@gmail.com>
2016-08-08 17:01:40 +0800

    修正小數點1.0 - 2.0的bug並開發縮小功能
    縮小功能弄錯了，for不能是縮小圖的次數
    這樣解析度會不足

--- CPP_Image/Hw01/ZeroOrder2_main.cpp
+++ CPP_Image/Hw01/ZeroOrder2_main.cpp
@@ -3,6 +3,9 @@ Name : ZeroOrder.cpp
 Date : 2016/08/03
 By   : CharlotteHonG
 Final: 2016/08/03
+
+已知小數點補法沒有平均
+black算法沒辦法平均
 **********************************************************/
 #include <iostream>
 #include <fstream>
@@ -10,50 +13,53 @@ Final: 2016/08/03
 #include "OpenRAW"
 using namespace std;
 
+#define AutoOpen 0
 #define Pic_name_in "IMG.raw"
 #define Pic_name_out "IMG_OUT.raw"
 #define Pic_x 256
 #define Pic_y 256
-#define Ratio 5.9
+#define Ratio 0.25
 
 int main(int argc, char const *argv[]) {
-    int aa=0;
+    if(Ratio < 0) {
+        cout << "Ratio more than the zero." << endl;
+        return 0;
+    }
     imgraw img(Pic_y, Pic_x);
-    imgraw img2(Pic_y*Ratio+aa, Pic_x*Ratio);
+    imgraw img2(Pic_y*Ratio, Pic_x*Ratio);
     img.read(Pic_name_in);
-
     //=========================================
-    int apend_p = Pic_x*(Ratio-1); // 需要補的點
-    int black, white; //幾個原點 (黑) 補一個新點 (白)
-    int black2=0; //處理放大倍率小數點，每幾個原點多補一個新點
-    int limx=0,limy=0;
-    int limx_c=0,limy_c=0;
+    int apend_p=0; // 需要補的點
+    int white=0; //幾個原點 (黑) 補一個新點 (白)
+    int black=0; //處理放大倍率小數點，每幾個原點多補一個新點
+    int limx=0,limy=0, limx_c=0,limy_c=0;
     int debug=0;
-    if(apend_p >= Pic_x) { //補點多
-        black = 1;
+
+    apend_p = Pic_x*(Ratio-1);
+    if(Ratio>1) {
         white = ((int)Ratio)-1;
-        if (((float)Ratio-(int)Ratio) >= 0){
-            black2 = 1/((float)Ratio-(int)Ratio);
-            limx=Pic_x*((float)Ratio-(int)Ratio);
-            limy=Pic_y*((float)Ratio-(int)Ratio);
+        if(((float)Ratio-(int)Ratio) > 0) {
+            black = 1/((float)Ratio-(int)Ratio);
+            cout << "**" << (int)Ratio <<endl;
+            limx = Pic_x * ((float)Ratio-(int)Ratio);
+            limy = Pic_y * ((float)Ratio-(int)Ratio);
         }
+
         // 單點操作
         for(int j=0, yp=0; j < Pic_y; ++j) {
-
             limx_c=0;
             for(int i=0, xp=0; i < Pic_x; ++i) {
                 // 填入原圖的點
                 img2.point_write(j+yp, i+xp, img.point_read(j, i));
-                
                 // 補點 (整數) 每經一圓點補多少新點
                 for(int k = 0; k < white; ++k) {
                     // k=補點位移指標，不是填入點位移指標
                     img2.point_write(j+yp, (i+xp)+k+1, img.point_read(j, i));
-                }xp+=white; //填入點位移指標
-
-                // 補點 2 (小數點倍率) 每經幾元點補一點新點
-                if ((i+1)%black2==0 && black2!=0){
-                    if (limx_c<limx){
+                } xp+=white; //填入點位移指標
+                // 補點 2 (小數點倍率) 每經幾原點補一點新點
+                if(black!=0 && (i+1)%black==0) { // 從0開始故多+1
+                    // debug++;
+                    if(limx_c<limx) {
                         limx_c+=1;
                         img2.point_write(j+yp, (i+xp)+1, img.point_read(j, i));
                         xp+=1;
@@ -62,50 +68,84 @@ int main(int argc, char const *argv[]) {
             }
 
             // 補排 (整數)
-            for (int i = 0; i < white; ++i){
-                for(int k=0; k < Pic_x*Ratio; ++k) 
+            for(int i = 0; i < white; ++i) {
+                for(int k=0; k < Pic_x*Ratio; ++k)
                     img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
                 // debug+=1;
-            }yp+=white; //填入排位移指標
-
+            } yp+=white; //填入排位移指標
             // 補排 2 (小數點倍率)
-            if ((j+1)%black2==0 && black2!=0){
-                if (limy_c<limy){
-                    for (int i = 0; i < white; ++i){
-                        if (limy_c<limy){
+            if(black!=0 && (j+1)%black==0) { // 從0開始故多+1
+                if(limy_c<limy) {
+                    if(white==0) { // 倍率介於1~2時
+                        if(limy_c<limy) {
                             limy_c+=1;
                             for(int k=0; k < Pic_x*Ratio; ++k)
-                                img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
-                            debug+=1;
+                                img2.point_write(j+yp+1, k, img2.point_read(j+yp, k));
+                        }
+                    }
+                    else {
+                        for(int i = 0; i < white; ++i) {
+                            if(limy_c<limy) {
+                                limy_c+=1;
+                                for(int k=0; k < Pic_x*Ratio; ++k)
+                                    img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
+                            }
+                            // debug+=1;
                         }
                     }
                     yp+=1; //填入排位移指標
                 }
             }
         }
+    } else { // 縮小
+        int jmp=3; // 每讀幾點跳過
+        // jmp = (1 / (1-(float)Ratio)) * (1-(float)Ratio);
+        cout << "jmp=" << jmp << endl;
 
+        for(int j = 0, yp = 0; j < (int)(Pic_y*Ratio); ++j){
+            // 列
+            for(int i = 0,xp = 0; i < (int)(Pic_x*Ratio); ++i){
+                if ((i+1)%jmp==0){// 每隔 jmp 點就跳過
+                    ++xp;
+                    debug++;
+                }
+                img2.point_write(j, i, img.point_read(j+yp, i+xp));
+            }img2.point_write(j, (int)(Pic_x*Ratio), img.point_read(j+yp, Pic_x));
+
+            // 排
+            if ((j+1)%jmp==0){
+                ++yp;
+            }
+        }
+        // 補最後一排
+        // for(int i = 0,xp = 0; i < (int)(Pic_x*Ratio); ++i){
+        //     if ((i+1)%jmp==0){// 每隔 jmp 點就跳過
+        //         ++xp;
+        //         debug++;
+        //     }
+        //     img2.point_write((int)(Pic_x*Ratio)-1, i, img.point_read(Pic_x-1, i+xp));
+        // }
+        // img2.point_write((int)(Pic_x*Ratio)-1, (int)(Pic_x*Ratio)-1,
+        //     img.point_read(Pic_y-1, Pic_x-1));
     }
-    else { //補點少
-        black = Pic_x/apend_p;
-        white = 1;
-    }
-    cout << "pix = " << apend_p+Pic_x << endl;
+
+    cout << "pix = " << (int)(Pic_x*Ratio) << endl;
     cout << "apend_p = "   << apend_p   << endl;
     cout << "Pic_x = "   << Pic_x   << endl;
-    cout << "black = "   << black   << endl;
     cout << "white = "   << white   << endl;
-    cout << "black2 = "   << black2   << endl;
+    cout << "black = "   << black   << endl;
     cout << "limx = "   << limx   << endl;
     cout << "limy = "   << limy   << endl;
     cout << "limx_c = "   << limx_c   << endl;
     cout << "limy_c = "   << limy_c   << endl;
-    cout << "debug = "   << debug   << endl;
+    cout << "debug = "   << debug/(int)(Pic_y*Ratio)   << endl;
     cout << "t = "   << ((float)Ratio-(int)Ratio)   << endl;
     //=========================================
 
-    
+
 
     img2.write(Pic_name_out);
-    system(Pic_name_out);
+    // if (AutoOpen==1)
+        system(Pic_name_out);
     return 0;
 }
---
+++ CPP_Image/Hw01/log.md
@@ -0,0 +1,27 @@
+## 問題
+macro的轉換似乎有著某種關係  
+
+```
+if (((float)Ratio-(int)Ratio) > 0){
+    black = 1/((float)Ratio-(int)Ratio);
+    limx = Pic_x * ((float)Ratio-(int)Ratio);
+    limy = Pic_y * ((float)Ratio-(int)Ratio);
+}
+```
+
+與  
+
+```
+if (float temp = ((float)Ratio-(int)Ratio) > 0){
+    black = 1/temp;
+    limx = Pic_x * temp;
+    limy = Pic_y * temp;
+}
+```
+
+居然是不一樣的  
+
+> macro小數轉換方法有待釐清，
+> 估計是小數捨棄方式不一樣
+> 或者是計算浮點數時會忽略些什麼
+> 注意以後不可以用 macro 計算非整數
