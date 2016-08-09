7d74c82b50cb10f866c19c7f38a45f6dcaeaa67d
Charlotte.HonG <hunandy14@gmail.com>
2016-08-09 18:14:41 +0800

    完成類別化 程式碼尚未整理 還有小bug
    1. 放大的方式不太平均
    2. 縮小的問題還沒修復
    
    最佳解決方案：改用算式來對應相對應的點...來不及了整個要打掉重來

--- CPP_Image/Hw01/OpenRAW.cpp
+++ CPP_Image/Hw01/OpenRAW.cpp
@@ -58,3 +58,127 @@ void imgraw::point_write(int y, int x, vector<char> value) {
     int pos = (y*this->width)+x;
     this->img_data[pos] = value[0];
 }
+
+// ZroOrder調整大小
+void imgraw::resize_zero(float Ratio){
+    //=========================================
+    float rat = (float)Ratio;
+    int Pic_x = this->width;
+    int Pic_y = this->high;
+    imgraw img2(Pic_y*Ratio, Pic_x*Ratio);
+
+    float rat_r = floor(rat);
+    int apend_p=0; // 需要補的點
+    int white=0; //幾個原點 (黑) 補一個新點 (白)
+    int black=0; //處理放大倍率小數點，每幾個原點多補一個新點
+    int limx=0,limy=0, limx_c=0,limy_c=0;
+    int debug=0;
+    apend_p = Pic_x*(rat-1);
+
+    if(rat>1) {
+        white = (rat_r)-1;
+        if((rat-rat_r) > 0) {
+            black = 1/(rat-rat_r);
+            cout << "   **" << (rat-rat_r) <<endl;
+            cout << "   black = " << black <<endl;
+            limx = Pic_x * (rat-rat_r);
+            limy = Pic_y * (rat-rat_r);
+        }
+
+        // 單點操作
+        for(int j=0, yp=0; j < Pic_y; ++j) {
+            limx_c=0;
+            for(int i=0, xp=0; i < Pic_x; ++i) {
+                // 填入原圖的點
+                img2.point_write(j+yp, i+xp, this->point_read(j, i));
+                // 補點 (整數) 每經一圓點補多少新點
+                for(int k = 0; k < white; ++k) {
+                    // k=補點位移指標，不是填入點位移指標
+                    img2.point_write(j+yp, (i+xp)+k+1, this->point_read(j, i));
+                } xp+=white; //填入點位移指標
+                // 補點 2 (小數點倍率) 每經幾原點補一點新點
+                if(black!=0 && (i+1)%black==0) { // 從0開始故多+1
+                    // debug++;
+                    if(limx_c<limx) {
+                        limx_c+=1;
+                        img2.point_write(j+yp, (i+xp)+1, this->point_read(j, i));
+                        xp+=1;
+                    }
+                }
+            }
+
+            // 補排 (整數)
+            for(int i = 0; i < white; ++i) {
+                for(int k=0; k < Pic_x*rat; ++k)
+                    img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
+                // debug+=1;
+            } yp+=white; //填入排位移指標
+            // 補排 2 (小數點倍率)
+            if(black!=0 && (j+1)%black==0) { // 從0開始故多+1
+                if(limy_c<limy) {
+                    if(white==0) { // 倍率介於1~2時
+                        if(limy_c<limy) {
+                            limy_c+=1;
+                            for(int k=0; k < Pic_x*rat; ++k)
+                                img2.point_write(j+yp+1, k, img2.point_read(j+yp, k));
+                        }
+                    }
+                    else {
+                        for(int i = 0; i < white; ++i) {
+                            if(limy_c<limy) {
+                                limy_c+=1;
+                                for(int k=0; k < Pic_x*rat; ++k)
+                                    img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
+                            }
+                            // debug+=1;
+                        }
+                    }
+                    yp+=1; //填入排位移指標
+                }
+            }
+        }
+    } else { // 縮小
+        int jmp=0; // 每讀幾點跳過
+        jmp = (1 / (1-rat));
+        cout << "jmp=" << jmp << endl;
+        limx = floor(Pic_x*rat);
+        limy = floor(Pic_y*rat);
+        for(int j = 0, yp = 0; j < Pic_y; ++j){
+            // 列
+            limx_c=0;
+            for(int i = 0,xp = 0; i < Pic_x; ++i){
+                if ((i+1)%jmp!=0){// 每隔 jmp 點就跳過
+                    img2.point_write(yp, xp, this->point_read(j, i));
+                    limx_c+=1;
+                    xp+=1;
+                    // 這裡的次數太少
+                }
+            }
+            // 排
+            if ((j+1)%jmp!=0 && limy_c< floor(Pic_x*rat)-1){
+                // if (limy_c<limy){
+                    limy_c+=1;
+                    yp+=1;
+                // }
+            }
+        }
+    }
+
+    cout << "pix = " << (int)(Pic_x*Ratio) << endl;
+    cout << "apend_p = "   << apend_p   << endl;
+    cout << "Pic_x = "   << Pic_x   << endl;
+    cout << "white = "   << white   << endl;
+    cout << "black = "   << black   << endl;
+    cout << "limx = "   << limx   << endl;
+    cout << "limy = "   << limy   << endl;
+    cout << "limx_c = "   << limx_c   << endl;
+    cout << "limy_c = "   << limy_c   << endl;
+    cout << "debug = "   << debug   << endl;
+    // cout << "t = "   << ((float)Ratio-(int)Ratio)   << endl;
+
+    // 將暫存寫回主檔
+    this->img_data=img2.img_data;
+    this->filesize=img2.filesize;
+    this->width=img2.width;
+    this->high=img2.high;
+}
--- CPP_Image/Hw01/ZeroOrder2_main.cpp
+++ CPP_Image/Hw01/ZeroOrder2_main.cpp
@@ -19,133 +19,22 @@ using namespace std;
 #define Pic_name_out "IMG_OUT.raw"
 #define Pic_x 256
 #define Pic_y 256
-#define Ratio 1
+#define Ratio 1.3
 
 int main(int argc, char const *argv[]) {
     if(Ratio < 0) {
         cout << "Ratio more than the zero." << endl;
         return 0;
     }
+    // 創建畫布
     imgraw img(Pic_y, Pic_x);
-    // imgraw img2(Pic_y, Pic_x);
-    imgraw img2(Pic_y*Ratio, Pic_x*Ratio);
+    // 讀檔
     img.read(Pic_name_in);
-    //=========================================
-    float rat = (float)Ratio;
-
-    float rat_r = floor(rat);
-    int apend_p=0; // 需要補的點
-    int white=0; //幾個原點 (黑) 補一個新點 (白)
-    int black=0; //處理放大倍率小數點，每幾個原點多補一個新點
-    int limx=0,limy=0, limx_c=0,limy_c=0;
-    int debug=0;
-    apend_p = Pic_x*(rat-1);
-
-    if(rat>1) {
-        white = (rat_r)-1;
-        if((rat-rat_r) > 0) {
-            black = 1/(rat-rat_r);
-            cout << "   **" << (rat-rat_r) <<endl;
-            cout << "   black = " << black <<endl;
-            limx = Pic_x * (rat-rat_r);
-            limy = Pic_y * (rat-rat_r);
-        }
-
-        // 單點操作
-        for(int j=0, yp=0; j < Pic_y; ++j) {
-            limx_c=0;
-            for(int i=0, xp=0; i < Pic_x; ++i) {
-                // 填入原圖的點
-                img2.point_write(j+yp, i+xp, img.point_read(j, i));
-                // 補點 (整數) 每經一圓點補多少新點
-                for(int k = 0; k < white; ++k) {
-                    // k=補點位移指標，不是填入點位移指標
-                    img2.point_write(j+yp, (i+xp)+k+1, img.point_read(j, i));
-                } xp+=white; //填入點位移指標
-                // 補點 2 (小數點倍率) 每經幾原點補一點新點
-                if(black!=0 && (i+1)%black==0) { // 從0開始故多+1
-                    // debug++;
-                    if(limx_c<limx) {
-                        limx_c+=1;
-                        img2.point_write(j+yp, (i+xp)+1, img.point_read(j, i));
-                        xp+=1;
-                    }
-                }
-            }
-
-            // 補排 (整數)
-            for(int i = 0; i < white; ++i) {
-                for(int k=0; k < Pic_x*rat; ++k)
-                    img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
-                // debug+=1;
-            } yp+=white; //填入排位移指標
-            // 補排 2 (小數點倍率)
-            if(black!=0 && (j+1)%black==0) { // 從0開始故多+1
-                if(limy_c<limy) {
-                    if(white==0) { // 倍率介於1~2時
-                        if(limy_c<limy) {
-                            limy_c+=1;
-                            for(int k=0; k < Pic_x*rat; ++k)
-                                img2.point_write(j+yp+1, k, img2.point_read(j+yp, k));
-                        }
-                    }
-                    else {
-                        for(int i = 0; i < white; ++i) {
-                            if(limy_c<limy) {
-                                limy_c+=1;
-                                for(int k=0; k < Pic_x*rat; ++k)
-                                    img2.point_write(j+yp+1+i, k, img2.point_read(j+yp, k));
-                            }
-                            // debug+=1;
-                        }
-                    }
-                    yp+=1; //填入排位移指標
-                }
-            }
-        }
-    } else { // 縮小
-        int jmp=0; // 每讀幾點跳過
-        jmp = (1 / (1-rat));
-        cout << "jmp=" << jmp << endl;
-        limx = floor(Pic_x*rat);
-        limy = floor(Pic_y*rat);
-        for(int j = 0, yp = 0; j < Pic_y; ++j){
-            // 列
-            limx_c=0;
-            for(int i = 0,xp = 0; i < Pic_x; ++i){
-                if ((i+1)%jmp!=0){// 每隔 jmp 點就跳過
-                    img2.point_write(yp, xp, img.point_read(j, i));
-                    limx_c+=1;
-                    xp+=1;
-                    // 這裡的次數太少
-                }
-            }
-            // 排
-            if ((j+1)%jmp!=0 && limy_c< floor(Pic_x*rat)-1){
-                // if (limy_c<limy){
-                    limy_c+=1;
-                    yp+=1;
-                // }
-            }
-        }
-    }
-
-    cout << "pix = " << (int)(Pic_x*Ratio) << endl;
-    cout << "apend_p = "   << apend_p   << endl;
-    cout << "Pic_x = "   << Pic_x   << endl;
-    cout << "white = "   << white   << endl;
-    cout << "black = "   << black   << endl;
-    cout << "limx = "   << limx   << endl;
-    cout << "limy = "   << limy   << endl;
-    cout << "limx_c = "   << limx_c   << endl;
-    cout << "limy_c = "   << limy_c   << endl;
-    cout << "debug = "   << debug   << endl;
-    // cout << "t = "   << ((float)Ratio-(int)Ratio)   << endl;
-    //=========================================
-
-
-
-    img2.write(Pic_name_out);
+    // 更改大小
+    img.resize_zero(Ratio);
+    // 寫檔
+    img.write(Pic_name_out);
+    // 開檔
     if (AutoOpen==1)
         system(Pic_name_out);
     return 0;
