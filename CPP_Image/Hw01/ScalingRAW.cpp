5c88fa8762e8d0666d5815f891353c09ca3a48d6
Charlotte.HonG <hunandy14@gmail.com>
2016-08-12 13:10:02 +0800

    完成firstorder

--- CPP_Image/Hw01/ScalingRAW.cpp
+++ CPP_Image/Hw01/ScalingRAW.cpp
@@ -9,11 +9,10 @@ void imgraw::resize_first(float Ratio){
     int w=floor(this->width * Ratio);
     int h=floor(this->high * Ratio);
     imgraw img2(h, w);
-    vector<char> A(1), B(1), C(1), D(1);//附近的四個點
-    vector<char> AB(1), CD(1), X(1), Y(1);
+    unsigned char A, B, C, D;//附近的四個點
+    unsigned char AB, CD, X;
     int oy, ox;//對應到原圖的座標
     double a, b;// 公式的a與b
-    int debug=0;
     for(int j=0; j < h; ++j) {
         for(int i=0; i < w; ++i) {
             oy=j/Ratio; ox=i/Ratio;
@@ -23,16 +22,12 @@ void imgraw::resize_first(float Ratio){
             D = this->point_read(oy+1, ox+1);
             a = (i-ox*Ratio)/(Ratio); 
             b = (j-oy*Ratio)/(Ratio);
-            AB[0] = (A[0]*(1-a)) + (B[0]*a);
-            CD[0] = (C[0]*(1-a)) + (D[0]*a);
-            X[0] = ((AB[0]*(1-b)) + (CD[0]*b));
-            // if (X[0]>255) X[0]=A[0];
-            // if (X[0]<0) X[0]=A[0];
+            AB = (A*(1.0-a)) + (B*a);
+            CD = (C*(1.0-a)) + (D*a);
+            X = ((AB*(1.0-b)) + (CD*b));
             img2.point_write(j, i, X);
         }
     }
-    cout << "debug=" << debug << endl;
-        
     *this = img2;
 }
 
@@ -62,11 +57,9 @@ imgraw::imgraw(int y, int x) {
 // 匯入檔案
 void imgraw::read(string filename) {
     this->filename = filename;
-    
     // 二進位模式開檔測試
     fstream img;
     img.open(filename, ios::in | ios::binary);
-    
     // 如果開啟檔案失敗，fp為0；成功，fp為非0
     if(!img) {
         img.close();
@@ -76,7 +69,6 @@ void imgraw::read(string filename) {
     else {
         cout << "File ok." << endl;
     } img.close();
-
     // 二進位模式讀檔
     // 取得總長
     img.open(this->filename, ios::in | ios::binary);
@@ -84,8 +76,8 @@ void imgraw::read(string filename) {
     this->filesize = img.tellg();
     img.seekg(0, ios::beg);
     // 讀取值
-    this->img_data.vector::resize(this->filesize);
-    img.fstream::read(&this->img_data[0], this->filesize);
+    this->img_data.resize(this->filesize);
+    img.read((char*)&this->img_data[0], this->filesize);
     img.close();
 }
 
@@ -94,22 +86,20 @@ void imgraw::write(string filename) {
     // 進位模式寫檔
     fstream img;
     img.open(filename, ios::out | ios::binary);
-    img.fstream::write(&img_data[0], this->filesize);
+    img.write((char*)&img_data[0], this->filesize);
     img.close();
 }
 
 // 讀檔單點
-vector<char> imgraw::point_read(int y, int x) {
+unsigned char imgraw::point_read(int y, int x) {
     int pos = (y*this->width)+x;
-    vector<char> temp(1);
-    temp[0] = this->img_data[pos];
-    return temp;
+    return this->img_data[pos];
 }
 
 // 寫入記憶體單點
-void imgraw::point_write(int y, int x, vector<char> value) {
+void imgraw::point_write(int y, int x, unsigned char value) {
     int pos = (y*this->width)+x;
-    this->img_data[pos] = value[0];
+    this->img_data[pos] = value;
 }
 
 // 調整畫布大小
@@ -118,12 +108,13 @@ void imgraw::resize_canvas(int y, int x) {
     this->high = y;
 }
 
-int imgraw::w(){
+// 獲得寬
+int imgraw::w() {
     return this->width;
 }
 
-int imgraw::h(){
+// 獲得高
+int imgraw::h() {
     return this->high;
-
 }
 //=========================================================
